d4bccb33b044d2f2217e553267223785a8e1b276
Bong Lee <bongster228@github.com>
2020-12-25 09:35:28 -0800

    project object validation. closes #4

--- server/src/main/java/com/pmt/prjmanagetool/controllers/ProjectController.java
+++ server/src/main/java/com/pmt/prjmanagetool/controllers/ProjectController.java
@@ -4,10 +4,14 @@ import com.pmt.prjmanagetool.domain.Project;
 import com.pmt.prjmanagetool.services.ProjectService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.HashMap;
 
 @RestController
 @RequestMapping(ProjectController.BASE_URL)
@@ -22,7 +26,17 @@ public class ProjectController {
     }
 
     @PostMapping()
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project) {
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
+
+        if (result.hasErrors()) {
+
+            HashMap<String, String> errors = new HashMap<>();
+            result.getFieldErrors().forEach(error -> {
+                errors.put(error.getField(), error.getDefaultMessage());
+            });
+
+            return new ResponseEntity<HashMap<String, String>>(errors, HttpStatus.BAD_REQUEST);
+        }
 
         Project savedProject = projectService.saveOrUpdateProject(project);
 
