4ee9b57266cdc570f4c44feec82e9c3a43c4b95a
Bong Lee <bongster228@github.com>
2020-12-25 08:54:34 -0800

    set up project object vaildation. closes #3

--- server/src/main/java/com/pmt/prjmanagetool/domain/Project.java
+++ server/src/main/java/com/pmt/prjmanagetool/domain/Project.java
@@ -1,12 +1,16 @@
 package com.pmt.prjmanagetool.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @EqualsAndHashCode(callSuper = true)
@@ -15,18 +19,26 @@ import java.util.Date;
 @Entity
 public class Project extends BaseEntity {
 
-
+    @NotBlank(message = "Project name is required")
     private String projectName;
 
+    @NotBlank(message = "Project Identifier is required")
+    @Size(min = 4, max = 5, message = "Please use 4 to 5 characters")
+    @Column(unique = true, updatable = false)
     private String projectIdentifier;
 
+    @NotBlank(message = "Project description is required")
     private String description;
 
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date startDate;
-
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date endDate;
 
+
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date createdAt;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date updatedAt;
 
     @PrePersist
