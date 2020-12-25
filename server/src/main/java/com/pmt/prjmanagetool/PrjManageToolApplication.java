67139e88febb2db26624a07bce6c1cc977056293
Bong Lee <bongster228@github.com>
2020-12-24 23:46:34 -0800

    initial commit for server

---
+++ server/src/main/java/com/pmt/prjmanagetool/PrjManageToolApplication.java
@@ -0,0 +1,13 @@
+package com.pmt.prjmanagetool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrjManageToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrjManageToolApplication.class, args);
+    }
+
+}
